30a688e7e72a4ebe830d3faf32feae7d78f6a036
Mert <79603812+ufcovo@users.noreply.github.com>
2023-08-13 16:07:15 +0300

    Code first project and usage of migration

---
+++ EFCore.CodeFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿using EFCore.CodeFirst;
+using EFCore.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} : {p.Name} : {p.Price} : {p.Stock}");
+    });
+}
