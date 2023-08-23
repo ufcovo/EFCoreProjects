ddd6099d056aad8509f372c6361af6410f4fc0a0
Mert <79603812+ufcovo@users.noreply.github.com>
2023-08-23 20:36:36 +0300

    one to mant add second way

--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -6,11 +6,12 @@ Initializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    var category = new Category() { Name = "Pencils" };
-    var product = new Product() { Name = "Pencil 1", Price = 100, Stock = 100, Barcode = 123, Category = category };
+    var category = new Category() { Name = "Books" };
+
+    category.Products.Add(new () { Name = "Book 1", Price = 100, Stock = 100, Barcode = 123 });
 
     //_context.Categories.Add(category); Buna gerek yok. Navigation property den direkt olarak eklenmekte.
-    _context.Products.Add(product);
+    _context.Add(category);
     _context.SaveChanges();
     Console.WriteLine("Saved");
 }
