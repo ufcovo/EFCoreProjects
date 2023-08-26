a0c5419ccdfa09ea03bf3170d60ec2f0aac4c386
Mert <79603812+ufcovo@users.noreply.github.com>
2023-08-26 13:37:38 +0300

    many to mant data add second way

--- EFCore.CodeFirst/Program.cs
+++ EFCore.CodeFirst/Program.cs
@@ -6,11 +6,21 @@ Initializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    var student = new Student() { Name = "Mert", Age = 25 };
-    student.Teachers.Add(new() { Name = "Teacher of Halim"});
-    student.Teachers.Add(new() { Name = "Teacher of Ahmet" });
+    //var student = new Student() { Name = "Mert", Age = 25 };
+    //student.Teachers.Add(new() { Name = "Teacher of Halim"});
+    //student.Teachers.Add(new() { Name = "Teacher of Ahmet" });
 
-    _context.Add(student);
+    var teacher = new Teacher()
+    {
+        Name = "Teacher of Mert",
+        Students = new() {
+        new() {Name = "Mert2", Age = 12},
+        new() {Name = "Mert3", Age = 11}
+        }
+    };
+
+
+    _context.Add(teacher);
     _context.SaveChanges();
 
     Console.WriteLine("Saved");
