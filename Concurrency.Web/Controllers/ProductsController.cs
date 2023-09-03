4172466c66feba05f26a020789f4f873f42430ed
Mert <79603812+ufcovo@users.noreply.github.com>
2023-09-03 18:46:59 +0300

    Create ProductUpdate page and showing RowVersion exception

---
+++ Concurrency.Web/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Concurrency.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Concurrency.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Product product)
+        {
+
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(List));
+        }
+    }
+}
