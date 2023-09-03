86c3cb24e0313de7d21c4660e7cd0ed59eb5a5d4
Mert <79603812+ufcovo@users.noreply.github.com>
2023-09-03 18:25:36 +0300

    API Project for Concurrency

---
+++ Concurrency.Web/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Concurrency.Web.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(r => r.RowVersion).IsRowVersion();
+            modelBuilder.Entity<Product>().Property(r => r.Price).HasPrecision(18, 2);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
